e11a1e4747f5574ddf26e793a33214cf00f1095d
sumannam <sumannam@gmail.com>
2021-10-12 22:28:48 +0900

    addCoupling 함수 개발 중

--- src/COUPLED_MODELS.py
+++ src/COUPLED_MODELS.py
@@ -12,7 +12,7 @@ from src.CO_ORDINATORS import CO_ORDINATORS
 
 class COUPLED_MODELS(MODELS):
     def __init__(self, model_name):
-        self.children = []
+        self.child_list = []
         MODELS.__init__(self, model_name)
         # PROCESSORS.__init__(self, model_name)
 
@@ -21,10 +21,31 @@ class COUPLED_MODELS(MODELS):
         self.processor.setDevsComponent(self)
     
     def addModel(self, child):
-        self.children.append(child)
+        self.child_list.append(child)
         child.setParent(self)
         child.getProcessor().setParent(self.getProcessor())
         
+    def existChildModel(self, child):
+        if(child==None):
+            return False
+    
+    def addCoupling(self, src_model, src_port, dst_model, dst_port):
+        if(src_model==dst_model):
+            print("Source Model and Destination Model of Addcoupling are the same.")
+            return False
+        
+        if(src_model!=self & self.existChildModel(src_model)==False):
+            print("Source Model of Addcoupling does not exist in the coupled model.")
+        
+        if(dst_model!=self & self.existChildModel(dst_model)==False):
+            print("Destination Model of Addcoupling does not exist in the coupled model.")
+
+        if(self.existChildModel(src_model) & self.existChildModel(dst_model)):
+
+            # RELATION 클래스 생성
+
+
+
 
 
         # self.setParent(self)
