6092142fa6f82a50dcd0d827d5acd2e1b877ccc4
smnam-duduit <sumannam@gmail.com>
2022-03-22 13:42:13 +0900

    개발 경로 설정

---
+++ projects/simparc/exam.py
@@ -0,0 +1,8 @@
+from path import app
+import os
+import sys
+# app()
+# def app():
+# sys.path.append('D:/Git/DEVS-Python')
+sys.path.append('D:/Git/DEVS-Python/projects/simparc')
+print(os.system("cd"))
---
+++ projects/simparc/path.py
@@ -0,0 +1,6 @@
+import sys
+
+
+def app():
+    sys.path.append('D:/Git/DEVS-Python')
+    sys.path.append('D:/Git/DEVS-Python/projects/simparc')
