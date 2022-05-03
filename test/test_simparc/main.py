a709103e86d333a7de86c7f6b3b7c0ecb0926610
sumannam <sumannam@gmail.com>
2022-05-03 22:26:27 +0900

    MD5를 활용하여 sim_msg_log 파일 비교 완료

--- test/test_simparc/main.py
+++ test/test_simparc/main.py
@@ -1,40 +1,27 @@
 from fileinput import filename
 import os
 import shutil
+import hashlib
 import sys
 import time
 
 from conf import setDevPath
 setDevPath()
 
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-# from projects.simparc.P import P
-
-# if __name__ == '__main__':
-#     p = P()
-
-#     ## 자동 테스트
-#     json_file = os.path.join(THIS_FOLDER, 'p_test.json')
-
-#     start = time.time()    
-#     p.runAutoModelTest(p, json_file)
-#     end = time.time()
-
-#     print(f"{end - start:.5f} sec")
-
-
-#     ## 수동 테스트
-#     p.runModelTest(p)
+index = PROJECT_PATH.find("test")
+THIS_PATH = PROJECT_PATH[:index] + "test\\test_simparc"
 
 def moveLogFile():
     file_name = 'sim_msg_log.txt'
-    index = THIS_FOLDER.find("test")
-    source_path = THIS_FOLDER[:index]
+    index = THIS_PATH.find("test")
+    source_path = THIS_PATH[:index]
+    destin = ""
 
     if index != -1:
         source = source_path + file_name
-        destin = THIS_FOLDER + "\\" + file_name
+        destin = THIS_PATH + "\\" + file_name
         
         while True:
             try:
@@ -42,7 +29,29 @@ def moveLogFile():
             except PermissionError:
                 continue
             break
-    
+
+    return destin
+
+def compareLogFile(source, destin):
+    md5_hash = hashlib.md5()
+
+    source_file = open(source, "rb")
+    destin_file = open(destin, "rb")
+
+    source_content = source_file.read()
+    destin_content = destin_file.read()
+
+    md5_hash.update(source_content)
+    md5_hash.update(destin_content)
+
+    source_digest = md5_hash.hexdigest()
+    destin_digest = md5_hash.hexdigest()
+
+    if source_digest == destin_digest:
+        return True
+    else:
+        return False
+
 
 # 시뮬레이션
 from EF_P import EF_P
@@ -51,4 +60,12 @@ if __name__ == '__main__':
     ef_p.initialize() 
     ef_p.restart()
     
-    moveLogFile()
+    destin = moveLogFile()
+
+    sim_msg_log_orig_file = 'sim_msg_log_orig.txt'
+    source = THIS_PATH + "\\" + sim_msg_log_orig_file
+
+    comp_result = compareLogFile(source, destin)
+
+    if comp_result == True:
+        print("sim_msg_log 파일 일치")
