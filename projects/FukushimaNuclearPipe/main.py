0b4167baf8df259557cdbb12a3766becc6b4bc4b
smnam-home <sumannam@gmail.com>
2024-09-10 20:28:46 +0900

    ms 단위 시간 측정 및 mqtt_time_log 파일 생성 완료

--- projects/FukushimaNuclearPipe/main.py
+++ projects/FukushimaNuclearPipe/main.py
@@ -1,25 +1,35 @@
+# import config
 import paho.mqtt.client as mqtt
 import queue
 import threading
+import os
 
-import config
+from datetime import datetime
 
+# 시뮬레이션
+from coupbase.EF_PIPE import EF_PIPE
+
+#
 from mqttMsg import MqttMsg
 
-# MQTT 설정 파일
+# MQTT
 MQTT_CLIENT_ID = "Simulator" # str
 MQTT_HOST = "localhost" # str
 MQTT_PORT = 1883 # int
 MQTT_USERID = None # str | None
 MQTT_USERPW = None # str | None
 
-# 시뮬레이션
-from coupbase.EF_PIPE import EF_PIPE
+def removeMQTTTimeLogFile():
+    if os.path.exists("mqtt_time_log.txt"):
+        os.remove("mqtt_time_log.txt")
+    else:
+        print("The file does not exist")
 
 if __name__ == '__main__':
     # 1. Create a message queue
     msgQueue = queue.Queue()
-    
+    removeMQTTTimeLogFile()
+
     # 2. Simulation -> wiil be run on sub-thread
     def RunSimulation():
         ef_piping = EF_PIPE(msgQueue)
@@ -55,8 +65,26 @@ if __name__ == '__main__':
         # Mqtt
         if type(msg) is MqttMsg:
             if mqttc.is_connected():
+                # 현재 시간 가져오기
+                now = datetime.now()
+                # 시간 형식을 '시:분:초.밀리초'로 출력
+                formatted_time = now.strftime("%H:%M:%S") + f".{int(now.microsecond / 1000):03d}"
+
+                # 시간 정보를 텍스트 파일에 기록
+                with open("mqtt_time_log.txt", "a") as file:
+                    file.write(f"{formatted_time} : 'topic': '{msg.topic}', 'payload': '{msg.payload}'\n")
+
                 mqttc.publish(msg.topic, msg.payload)
+            
             else: # For Test
+                # # 현재 시간 가져오기
+                # now = datetime.now()
+                # # 시간 형식을 '시:분:초.밀리초'로 출력
+                # formatted_time = now.strftime("%H:%M:%S") + f".{int(now.microsecond / 1000):03d}"
+
+                # # 시간 정보를 텍스트 파일에 기록
+                # with open("mqtt_time_log.txt", "a") as file:
+                #     file.write(f"{formatted_time} : 'topic': '{msg.topic}', 'payload': '{msg.payload}'\n")
                 print(f"---mqtt---> 'topic': '{msg.topic}', 'payload': '{msg.payload}'")
         else: # For Test
             print(f"NOT `MqttMsg`: {type(msg)}: '{msg}'")
@@ -66,3 +94,4 @@ if __name__ == '__main__':
     mqttc.loop_stop()
     #
     print("---Done---")
+
