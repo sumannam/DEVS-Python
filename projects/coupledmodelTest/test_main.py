b608f89ef4572276bcef229ea59ed4a72beeb38b
smnam-home <sumannam@gmail.com>
2025-01-21 09:25:00 +0900

    CoupledModelTest 프로젝트 추가

---
+++ projects/coupledmodelTest/test_main.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+import config
+
+from projects.simparc.mbase.IP import IP
+from src.TEST_ATOMIC_MODELS import TEST_ATOMIC_MODELS
+
+if __name__ == '__main__':
+    ip = IP()
+    model_test = TEST_ATOMIC_MODELS()
+    
+    # 자동 테스트
+    json_file = os.path.join(config.TBASE_FOLDER, 'test_script1.json')
+
+    start = time.time() 
+
+    model_test.runAutoModelTest(ip, json_file)
+    end = time.time()
+
+    print(f"{end - start:.5f} sec")
+
+    ## 수동 테스트
+    # ip.modelTest(ip)
