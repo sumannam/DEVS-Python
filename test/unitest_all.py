61cb58cbff4f20c1b64b55e8f14cc1c66a3e8c71
smnam-home <sumannam@gmail.com>
2025-02-01 07:53:51 +0900

    unitest를 위한 파일 이름 변경
    https://github.com/sumannam/DEVS-Python/issues/34#issuecomment-2628529805

--- test/unitest_all.py
+++ test/unitest_all.py
@@ -13,5 +13,5 @@ def test_build(file_name):
         print(f'{file_name} 빌드 실패')
 
 if __name__ == "__main__":
-    # test_build('test\\test_engine.py')
-    test_build('test\\test_models.py')
+    # test_build('test\\unitest_engine.py')
+    test_build('test\\unitest_models.py')
