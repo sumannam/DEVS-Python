65dd94737e18747c54f524a7f7f3fcf626ec818b
smnam-home <sumannam@gmail.com>
2025-02-14 14:20:20 +0900

    import 참고하는 경로 수정

--- test/pytest/pytest_SENSORS.py
+++ test/pytest/pytest_SENSORS.py
@@ -2,7 +2,9 @@ import sys
 import os
 import pytest
 
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+# 프로젝트 루트 디렉토리를 파이썬 경로에 추가
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(project_root)
 
 from projects.coupledmodelTest.coupbase.SENSORS import SENSORS
 
